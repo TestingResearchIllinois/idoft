402b93fcc9c9cf0e02795d914b009c8550da2227
FanYuliang <yf3@illinois.edu>
2021-12-07 00:44:42 -0600

    debug done

--- auto-check-deleted/find_all_deleted.py
+++ auto-check-deleted/find_all_deleted.py
@@ -14,7 +14,6 @@ df = pd.read_csv("../pr-data.csv")
 error_log = []
 test_file_deleted = []
 test_method_deleted = []
-df_copy = df.copy()
 base_dir = os.getcwd()
 
 df = df.groupby("Project URL").agg(lambda x: list(x))
@@ -22,18 +21,16 @@ df = df.groupby("Project URL").agg(lambda x: list(x))
 for url, row in tqdm(df.iterrows(), total=df.shape[0]):
 	if url in archieved:
 		continue
-	testname_list, status_list = row["Fully-Qualified Test Name (packageName.ClassName.methodName)"], row["Status"]
+	testname_list, status_list, note_list = row["Fully-Qualified Test Name (packageName.ClassName.methodName)"], row["Status"], row["Notes"]
 	print("url", url)
 
 	clone_cmd = "git clone --quiet {} tmp && cd tmp".format(url)
-	process = subprocess.run(clone_cmd, shell=True, stdout=open(os.devnull, 'wb'))
+	subprocess.run(clone_cmd, shell=True, stdout=open(os.devnull, 'wb'))
 	print("clone done!!!")
-	for full_testname, status in zip(testname_list, status_list):
+	for full_testname, status, note in zip(testname_list, status_list, note_list):
 		testfile_name, test_method_name = full_testname.split(".")[-2], full_testname.split(".")[-1]
 		
-		if status == "RepoArchived" or status == "Deleted":
-			continue
-		else:
+		if pd.isna(status) and pd.isna(note):
 			
 			if "[" in test_method_name or "\"" in test_method_name:
 				print("{}, {} is a PUT; skip".format(url, full_testname))
@@ -44,42 +41,27 @@ for url, row in tqdm(df.iterrows(), total=df.shape[0]):
 				
 			fnames = glob("**/{}.java".format(testfile_name), recursive=True)
 			if len(fnames) == 0:
-				# test file not found; add to log
-				test_file_deleted.append("{}, {}".format(url, full_testname))
-				print("[INFO]: detected deleted Test File: ", "{}, {}".format(url, full_testname))
-			else:
-				if len(fnames) > 1:
-					print("{}, {} has multiple files with same test file name!".format(url, full_testname))
-				for fname in fnames:
-					with open(fname) as f:
-						found = False
-						for line in f.readlines():
-							# make sure it's a method
-							if test_method_name + "(" in line: 
-								found = True
-								break
+				# try to get the commit id
+				git_find_SHA = "cd tmp && git rev-list -n 1 HEAD -- **/{}.java".format(testfile_name)
+				commit_SHA = subprocess.check_output(git_find_SHA, shell=True).decode("utf-8")
 
-						if not found:
-							print("[INFO]: detected deleted Test Method: ", "{}, {}".format(url, full_testname))
-							test_method_deleted.append("{}, {}".format(url, full_testname))
-							df_copy.loc[df_copy["Fully-Qualified Test Name (packageName.ClassName.methodName)"] == full_testname, "Status"] = "Deleted"
-							# find the commit that delete the test using git-pickaxe
-							# @TODO here
+				if len(commit_SHA) > 0:
+					test_file_deleted.append("{}, {}, {}".format(url, full_testname, url+"/commit/"+commit_SHA))
+					print("[INFO]:detected Test File " + "{}, {} was deleted at SHA {}".format(url, full_testname, commit_SHA))
+				# test file not found; add to log
+				else:
+					test_file_deleted.append("{}, {}".format(url, full_testname))
+					print("[INFO]: detected deleted Test File: ", "{}, {}".format(url, full_testname))
+
 
 	# clean the repo and repeat
 	os.chdir(base_dir)
 	shutil.rmtree("tmp")
 
-# save the record
-df_copy.to_csv("new_pr_data.csv")
-with open("error_log.json", "w") as f:
-	json.dump(error_log, f)
 
-with open("test_file_deleted.json", "w") as f:
+with open("new_test_file_deleted.json", "w") as f:
 	json.dump(test_file_deleted, f)
 
-with open("test_method_deleted.json", "w") as f:
-	json.dump(test_method_deleted, f)
 
 
 
