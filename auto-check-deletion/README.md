30059d43d2ef8e51bc66ce0cfc01070a86003860
sopan98 <phaltankarsopan@gmail.com>
2022-12-10 19:56:04 -0600

    Update script and add README.md

---
+++ auto-check-deletion/README.md
@@ -0,0 +1,18 @@
+## checkCommitForDeletion
+
+If you find a commit that has `Deleted` a flaky test, you can use this script to verify your findings.
+
+### Usage
+
+1. Some filters need to be specified to `filter-tests.sh` in order for it to filter the contents of `pr-data.csv`. 
+
+2. Example usage: `./filter-tests.sh Deleted RocksDbPartition`. In this command, the script will output a file that is `Deleted` and contains `RocksDbPartition` in its test name. `filtered_tests.txt` is generated as an output which contains the test name and the commit link.
+
+3. Any number of arguments can be provided to this script. They need to be space separated.
+
+4. Copy the script `check-deletion.sh` and `filtered_tests.txt` to the directory of the project.
+
+3. Run the script with `bash check-deletion.sh` or `./check-deletion.sh`
+
+The output of this execution will show whether a test method or class has been deleted in that commit.
+It will also be stored in a file `check-deletion-output.txt` in the directory where you run the script.
--- auto-check-deletion/check-deletion.sh
+++ auto-check-deletion/check-deletion.sh
@@ -2,6 +2,13 @@
 
 filtered_tests=$(cat filtered_tests.txt | sed 's/,.*\//,/' | rev | cut -d'.' -f 1,2 | rev);
 
+output_file='check-deletion-output.txt';
+
+# Check the file is exists or not
+if [ -f $output_file ]; then
+   rm check-deletion-output.txt;
+fi
+
 IFS=','
 
 printf "$filtered_tests" | while read line;
@@ -13,12 +20,14 @@ do
 	class=$(printf "$test" | cut -d'.' -f 1);
 	test_method=$(printf "$test" | cut -d'.' -f 2);
 	git_show=$(git show "${commit::-1}");
-	echo ==== deleted ${test} ? ====;
+	echo ==== deleted ${test} ? ==== |& tee -a $output_file;
 	if echo "$git_show" | grep -q "^-.*${test_method}"
 	then
-		echo "$git_show" | grep "^-.*${test_method}";
+		echo "$git_show" | grep "^-.*${test_method}" |& tee -a $output_file;
 	else
-		echo "Deleted method not found. Finding deleted test class."
-		echo "$git_show" | grep "^-.*${class}";
+		echo "Deleted method not found. Finding deleted test class." |& tee -a $output_file;
+		echo "$git_show" | grep "^-.*${class}" |& tee -a $output_file;
 	fi
 done
+
+echo "$output_file";
