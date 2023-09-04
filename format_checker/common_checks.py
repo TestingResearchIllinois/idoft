372dc09420fd6418c124b7a614b9086e5d271461
Elen Chatikyan <echatikyan@gmail.com>
2023-09-04 15:19:18 -0500

    Minor fix

--- format_checker/common_checks.py
+++ format_checker/common_checks.py
@@ -92,7 +92,7 @@ def check_sort(filename, log):
     diff = subprocess.check_output(command, shell=True).decode("utf-8")
     if diff != "":
         log_esp_error(filename, log, "The file is not properly ordered")
-        print("Refer to IDoFT readme for how " + filename, " should be sorted: https://github.com/TestingResearchIllinois/idoft#to-contribute-a-newly-detected-flaky-test")
+        print("Refer to IDoFT readme for how " + filename + " should be sorted: https://github.com/TestingResearchIllinois/idoft#to-contribute-a-newly-detected-flaky-test")
         print("Differences between current order and expected order:")
         print(diff)
 
