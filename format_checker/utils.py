62d4742c9142d583f549c7f6df5056f6630c44b1
Elen Chatikyan <echatikyan@gmail.com>
2023-09-25 17:20:34 -0500

    Fixed flake8 warnings: blank lines, white spaces.

--- format_checker/utils.py
+++ format_checker/utils.py
@@ -92,7 +92,7 @@ def log_info(filename, log, message):
     log.info("INFO: On file " + filename + ": " + message)
 
 
-def log_std_error(filename, log, line, row, key, message = "Unspecified"):
+def log_std_error(filename, log, line, row, key, message="Unspecified"):
     """Logs a standard error."""
 
     log_std_error.tracker += 1
