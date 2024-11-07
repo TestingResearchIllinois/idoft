4dbe82c56e2b6745f15ced74442d27ec6e066500
zhihao11ui <zhihao11@illinois.edu>
2024-11-07 00:52:24 -0600

    Update unmaintained status to py-data and minor fix to script

--- auto_check_unmaintained/mark_repo_unmaintained.py
+++ auto_check_unmaintained/mark_repo_unmaintained.py
@@ -6,13 +6,13 @@ last_commit_dict = unmaintained_repos_df.set_index('REPO_URL')['LAST_COMMIT_DATE
 
 
 def update_row(row):
-    if row['ProjectURL'] in last_commit_dict:
+    if row['Project URL'] in last_commit_dict:
         if pd.isna(row['Status']):  # Check if 'Status' is empty
             row['Status'] = 'Unmaintained'
         else:
             return row
 
-        row['Notes'] = f"{row['Notes']} | {last_commit_dict[row['ProjectURL']]}" if pd.notna(row['Notes']) else f"{last_commit_dict[row['ProjectURL']]}"
+        row['Notes'] = f"{row['Notes']} | {last_commit_dict[row['Project URL']]}" if pd.notna(row['Notes']) else f"{last_commit_dict[row['Project URL']]}"
     return row
 
 
