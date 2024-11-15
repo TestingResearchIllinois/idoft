f81bc44ee012a0dd1ab7f9243ac4275e9ab8c81c
Weronika Trzaska <wtrzas2@illinois.edu>
2024-11-14 21:09:41 -0600

    Updated git bisect scripts to accept more arguments
    Update git bisect runner arguments order

--- auto-check-fix-commit/git-bisect-script.sh
+++ auto-check-fix-commit/git-bisect-script.sh
@@ -4,14 +4,16 @@ echo "Started Execution"
 
 test_module=$1
 test_case=$2
+nondex_version=$3
+mvn_options=$4
 
 echo "Started Build"
-mvn clean install -pl $test_module -am -DskipTests -Dlicense.skip
+mvn clean install -pl $test_module -am -DskipTests -Dlicense.skip $mvn_options
 if [[ "$?" -ne 0 ]]; then
   echo "Build Failed"
   exit 1
 else
-  mvn -pl $test_module edu.illinois:nondex-maven-plugin:1.1.2:nondex -Dtest=$test_case -DnondexRuns=5 -Dlicense.skip=true
+  mvn -pl $test_module edu.illinois:nondex-maven-plugin:$nondex_version:nondex -Dtest=$test_case -DnondexRuns=5 -Dlicense.skip=true
   if [[ "$?" -ne 0 ]]; then
     echo "NonDex Test Failed"
     exit 0
