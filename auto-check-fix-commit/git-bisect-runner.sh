cd7fa2fba6f6c0aee41e887471dfdd45de4ef61f
Weronika Trzaska <wtrzas2@illinois.edu>
2024-11-14 21:09:41 -0600

    Updated git bisect scripts to accept more arguments
    Update git bisect runner arguments order

--- auto-check-fix-commit/git-bisect-runner.sh
+++ auto-check-fix-commit/git-bisect-runner.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-for arg in "$@"; do
-  shift
-  case "$arg" in
-  '--flaky') set -- "$@" '-f' ;;
-  '--fixed') set -- "$@" '-x' ;;
-  '--module') set -- "$@" '-m' ;;
-  '--test') set -- "$@" '-t' ;;
-  *) set -- "$@" "$arg" ;;
+mvn_options=""
+nondex_version=""
+flaky_commit=""
+fixed_commit=""
+test_module=""
+test_case=""
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  '--flaky') flaky_commit="$2"; shift 2 ;;
+  '--fixed') fixed_commit="$2"; shift 2 ;;
+  '--module') test_module="$2"; shift 2 ;;
+  '--test') test_case="$2"; shift 2 ;;
+  '--nondex-version') nondex_version="$2"; shift 2 ;;
+  '--mvn-install') mvn_options="$2"; shift 2 ;;
+  *) echo "Unknown argument: $1"; exit 1 ;;
   esac
 done
 
-OPTIND=1
-while getopts "f:x:m:t:" opt; do
-  case "$opt" in
-  'f') flaky_commit=$OPTARG ;;
-  'x') fixed_commit=$OPTARG ;;
-  'm') test_module=$OPTARG ;;
-  't') test_case=$OPTARG ;;
-  esac
-done
-shift $(expr $OPTIND - 1)
+echo "Flaky Commit: $flaky_commit"
+echo "Fixed Commit: $fixed_commit"
+echo "Test Module: $test_module"
+echo "Test Case: $test_case"
+echo "NonDex Version: $nondex_version"
+echo "Maven Options: $mvn_options"
+
+if [[ -z "$flaky_commit" || -z "$fixed_commit" || -z "$test_module" || -z "$test_case" || -z "$nondex_version" ]]; then
+  echo "Error: Missing required argument(s)."
+  exit 1
+fi
 
 git checkout $fixed_commit
 
@@ -32,4 +41,4 @@ git checkout $flaky_commit
 
 git bisect good
 
-git bisect run ./git-bisect-script.sh $test_module $test_case
+git bisect run ./git-bisect-script.sh $test_module $test_case "$nondex_version" "$mvn_options"
